bce3278c41f35c389d8fb0578b5e11164cc8ded9
Alikhan <alikhan.tursunbekov@gmail.com>
2023-10-17 22:00:06 +0600

    Finished UI things

--- Neobis_iOS_StopWatch/ViewController.swift
+++ Neobis_iOS_StopWatch/ViewController.swift
@@ -23,6 +23,63 @@ class ViewController: UIViewController {
         segmentController.translatesAutoresizingMaskIntoConstraints = false
         return segmentController
     }()
+    
+    let timerText: UILabel = {
+        let text = UILabel()
+        text.text = "00:00:00"
+        text.textAlignment = .center
+        text.textColor = .black
+        text.font = UIFont.boldSystemFont(ofSize: 70)
+        text.translatesAutoresizingMaskIntoConstraints = false
+        return text
+    }()
+    
+    var stopButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    var stopImage: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(systemName: "stop.circle.fill")
+        image.contentMode = .scaleAspectFit
+        image.tintColor = .black
+        image.translatesAutoresizingMaskIntoConstraints = false
+        
+        return image
+    }()
+    
+    var startButton: UIButton =  {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    var startImage: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(systemName: "play.circle.fill")
+        image.contentMode = .scaleAspectFit
+        image.tintColor = .black
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    var pauseButton: UIButton =  {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    var pauseImage: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(systemName: "pause.circle.fill")
+        image.contentMode = .scaleAspectFit
+        image.tintColor = .black
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,6 +93,13 @@ class ViewController: UIViewController {
     func setupConstraints() {
         view.addSubview(timerImage)
         view.addSubview(segmentController)
+        view.addSubview(timerText)
+        view.addSubview(stopButton)
+        stopButton.addSubview(stopImage)
+        view.addSubview(startButton)
+        startButton.addSubview(startImage)
+        view.addSubview(pauseButton)
+        pauseButton.addSubview(pauseImage)
         
         NSLayoutConstraint.activate([
             timerImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
@@ -44,7 +108,40 @@ class ViewController: UIViewController {
             timerImage.heightAnchor.constraint(equalToConstant: heightD(100)),
             
             segmentController.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            segmentController.topAnchor.constraint(equalTo: timerImage.bottomAnchor, constant: heightD(20))
+            segmentController.topAnchor.constraint(equalTo: timerImage.bottomAnchor, constant: heightD(20)),
+            
+            timerText.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            timerText.topAnchor.constraint(equalTo: segmentController.bottomAnchor, constant: heightD(50)),
+            
+            pauseButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            pauseButton.topAnchor.constraint(equalTo: timerImage.bottomAnchor, constant: heightD(450)),
+            pauseButton.heightAnchor.constraint(equalToConstant: heightD(80)),
+            pauseButton.widthAnchor.constraint(equalToConstant: heightD(80)),
+            
+            pauseImage.centerXAnchor.constraint(equalTo: pauseButton.centerXAnchor),
+            pauseImage.centerYAnchor.constraint(equalTo: pauseButton.centerYAnchor),
+            pauseImage.heightAnchor.constraint(equalToConstant: heightD(80)),
+            pauseImage.widthAnchor.constraint(equalToConstant: heightD(80)),
+            
+            stopButton.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: widthD(-100)),
+            stopButton.topAnchor.constraint(equalTo: timerImage.bottomAnchor, constant: heightD(450)),
+            stopButton.heightAnchor.constraint(equalToConstant: heightD(80)),
+            stopButton.widthAnchor.constraint(equalToConstant: heightD(80)),
+            
+            stopImage.centerXAnchor.constraint(equalTo: stopButton.centerXAnchor),
+            stopImage.centerYAnchor.constraint(equalTo: stopButton.centerYAnchor),
+            stopImage.heightAnchor.constraint(equalToConstant: heightD(80)),
+            stopImage.widthAnchor.constraint(equalToConstant: heightD(80)),
+            
+            startButton.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: widthD(100)),
+            startButton.topAnchor.constraint(equalTo: timerImage.bottomAnchor, constant: heightD(450)),
+            startButton.heightAnchor.constraint(equalToConstant: heightD(80)),
+            startButton.widthAnchor.constraint(equalToConstant: heightD(80)),
+            
+            startImage.centerXAnchor.constraint(equalTo: startButton.centerXAnchor),
+            startImage.centerYAnchor.constraint(equalTo: startButton.centerYAnchor),
+            startImage.heightAnchor.constraint(equalToConstant: heightD(80)),
+            startImage.widthAnchor.constraint(equalToConstant: heightD(80)),
         ])
     }
     

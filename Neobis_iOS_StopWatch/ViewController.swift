91ae0a75f0d8963b01c213e4c64f5bd642dd4db6
Alikhan <alikhan.tursunbekov@gmail.com>
2023-10-17 20:56:25 +0600

    Initial commit

---
+++ Neobis_iOS_StopWatch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Neobis_iOS_StopWatch
+//
+//  Created by Alikhan Tursunbekov on 17/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.yellow
+    }
+
+
+}
+
